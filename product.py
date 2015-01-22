bed8a3b8eb1869ed360c0a3472d954057b5992f1
Jan Vereecken <janvereecken@clubit.be>
2015-01-22 13:58:08 +0100

    Allow products to be sold, even if there is no availability. Customers are being informed via mail that their order is not going to be shipped immediately.

--- product.py
+++ product.py
@@ -77,8 +77,6 @@ class product_product(osv.Model):
 
                 if product.state == 'obsolete' or len(product.seller_ids) == 0 or not product.cost_price or product.supplier_storage_location != '1015':
                     vals = {'sale_ok' : False, 'purchase_ok' : False}
-                elif not [x for x in product.seller_ids if x.state in ('available','limited') and x.product_code]:
-                    vals = {'sale_ok' : False, 'purchase_ok' : False}
                 elif product.state == 'draft':
                     vals = {'sale_ok' : False, 'purchase_ok' : True}
                 elif product.state == 'end':
